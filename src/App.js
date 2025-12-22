46decd48ce4648ff284c38ded653dfb427913a83
pavanreddy72-ai <pavankevin72@gmail.com>
2025-12-22 19:37:42 +0530

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-// import logo from './logo.svg';
-// import './App.css';
-import Register from './Component/Register.jsx'; 
-
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./Component/Navbar";
+import Home from "./Component/Homepage.jsx";
+import Login from "./Component/Login.jsx";
+import Register from "./Component/Register.jsx";
 function App() {
   return (
-    <>
-    <Register/>
-    </>
+    <Router>
+      <Navbar /> 
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
   );
 }
 
